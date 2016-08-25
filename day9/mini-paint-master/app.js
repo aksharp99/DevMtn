d2e75105c7d441f0ccd95f574f957913d922a426
akshar <aksharp99@gmail.com>
2016-08-25 16:45:18 -0600

    Day nine project done

---
+++ day9/mini-paint-master/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+$('.box').on('click',function(){
+
+  $('.box').on('click',function(){
+    $(this).addClass('white');
+  })
+  $('.box').on('dblclick',function(){
+    $(this).removeClass('white');
+  })
+
+})
+})
