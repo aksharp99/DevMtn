f9eea30873c6f4aa05104cdbd9831cf76a8b5153
akshar <aksharp99@gmail.com>
2016-08-31 12:34:52 -0600

    Finish http mini project

--- day13/mini-userProfiles2-master/controller.js
+++ day13/mini-userProfiles2-master/controller.js
@@ -1,10 +1,35 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
 
+  // $scope.getUsers = function() {
+  // 	mainService.getUsers().then(function(response) {
+  //     $scope.users = response.data.data;
+  //   });
+  // }
+  $scope.pageNum = 1;
+
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+    var promise = mainService.getUsers($scope.pageNum);
+
+    promise.then(function(response){
+      $scope.users = response.data.data;
+      $scope.totalPages = response.data.total_pages;
+    })
   }
 
+$scope.getUsers();
 
-  $scope.getUsers();
+  $scope.prev = function() {
+    if($scope.pageNum > 1) {
+    $scope.pageNum--;
+    $scope.getUsers();
+  }
+  }
+
+  $scope.next = function() {
+    if($scope.totalPages > $scope.pageNum){
+    $scope.pageNum++;
+    $scope.getUsers();
+  }
+  }
 
 });
