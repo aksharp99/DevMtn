eae88c0f89f2c6c6e97879b843c1c908e3b72565
akshar <aksharp99@gmail.com>
2016-09-26 20:06:32 -0600

    friends and first angular commit

---
+++ mini-MyFirstAngular/js/mainCtrl.js
@@ -0,0 +1,27 @@
+angular.module('friendsList').controller('mainCtrl', function($scope) {
+  $scope.name = "Akshar";
+  $scope.listOfFriends = ["Ken","Nick","Tunde","Rahul","Minh","Phu","Alex"];
+
+  $scope.x = angular.element(document.querySelector('#special'));
+  $scope.test = $scope.x;
+
+  $scope.test.on('click', function() {
+
+    var i = Math.floor(Math.random() * $scope.listOfFriends.length);
+
+  $scope.alldesefriends = $scope.listOfFriends[i];
+
+console.log($scope.alldesefriends);
+
+})
+  $scope.addFriend = function(anotherFriend) {
+    console.log(anotherFriend);
+    return $scope.listOfFriends.push(anotherFriend);
+
+  }
+
+$scope.salutation="Hello";
+$scope.name = "World";
+
+
+})
