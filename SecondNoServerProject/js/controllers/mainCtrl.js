252a98dc49cb77492f17907e7a85dbc05c85164c
akshar <aksharp99@gmail.com>
2016-10-08 17:14:12 -0600

    Initial

--- SecondNoServerProject/js/controllers/mainCtrl.js
+++ SecondNoServerProject/js/controllers/mainCtrl.js
@@ -1,10 +1,32 @@
-angular.module('journal').controller('mainCtrl', function($scope) {
+angular.module('journal').controller('mainCtrl', function($scope, mainService) {
   $scope.getApi = function() {
-    mainService.getApi().then(function(response) {
-      $scope.api = response.data;
-      console.log($scope.api)
-      console.log(response.data);
+    mainService.getApi('www.microsoft.com').then(function(response) {
+
+      $scope.fromlat = response.data.routes[0].bounds.northeast.lat;
+
+      $scope.fromlng = response.data.routes[0].bounds.northeast.lng;
+
+      $scope.tolat = response.data.routes[0].bounds.southwest.lat;
+
+      $scope.tolng = response.data.routes[0].bounds.southwest.lng;
+
+
+    $scope.getSteps = function() {
+      $scope.steps = response.data.routes[0].legs[0].steps;
+        console.log(response.data.routes[0].legs[0].steps);
+    }
+
+
     })
-    $scope.getApi();
+
   }
+
+  $scope.getAnotherApi = function(frominput, toinput) {
+    mainService.getAnotherApi(frominput, toinput).then(function(response) {
+      $scope.steps = response.data.routes[0].legs[0].steps;
+
+    })
+  }
+      $scope.getApi();
+      // $scope.getAnotherApi();
 })
--- SecondNoServerProject/js/services/mainService.js
+++ SecondNoServerProject/js/services/mainService.js
@@ -1,8 +1,23 @@
 angular.module('journal').service('mainService', function($http) {
-  this.getApi = function() {
+  this.getApi = function(site) {
     return $http({
       method: 'GET',
-      url: freegeoip.net/json/
+      url: 'https://maps.googleapis.com/maps/api/directions/json?origin=75+9th+Ave+New+York,+NY&destination=MetLife+Stadium+1+MetLife+Stadium+Dr+East+Rutherford,+NJ+07073&key=AIzaSyDIYdOLEBDJLUV6pd41h_PAdVhFoXZyyTo'
+
     })
   }
+
+this.getAnotherApi = function(originname, destname) {
+
+  var key = 'AIzaSyDIYdOLEBDJLUV6pd41h_PAdVhFoXZyyTo';
+  for (var i = 0; i < destname.length; i++) {
+    if(destname.slice(i, i+1) === " ") {
+      destname = destname.slice(0,i) + "&" + destname.slice(i + 1);
+    }
+  }
+  return $http({
+    method: 'GET',
+    url:'https://maps.googleapis.com/maps/api/directions/json?origin='+originname+'&destination='+destname+'&key='+key
+  })
+}
 })
