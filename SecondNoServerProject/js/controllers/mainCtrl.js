b9fcf7b8b6baa18d55bc0cece76f8e067a922031
akshar <aksharp99@gmail.com>
2016-10-09 11:02:20 -0600

    Initial

--- SecondNoServerProject/js/controllers/mainCtrl.js
+++ SecondNoServerProject/js/controllers/mainCtrl.js
@@ -1,32 +1,18 @@
 angular.module('journal').controller('mainCtrl', function($scope, mainService) {
-  $scope.getApi = function() {
-    mainService.getApi('www.microsoft.com').then(function(response) {
-
-      $scope.fromlat = response.data.routes[0].bounds.northeast.lat;
-
-      $scope.fromlng = response.data.routes[0].bounds.northeast.lng;
-
-      $scope.tolat = response.data.routes[0].bounds.southwest.lat;
-
-      $scope.tolng = response.data.routes[0].bounds.southwest.lng;
 
 
-    $scope.getSteps = function() {
+  $scope.directionApi = function(frominput, toinput) {
+    mainService.directionApi(frominput, toinput).then(function(response) {
       $scope.steps = response.data.routes[0].legs[0].steps;
-        console.log(response.data.routes[0].legs[0].steps);
-    }
-
-
-    })
-
-  }
-
-  $scope.getAnotherApi = function(frominput, toinput) {
-    mainService.getAnotherApi(frominput, toinput).then(function(response) {
-      $scope.steps = response.data.routes[0].legs[0].steps;
-
+      console.log($scope.steps);
+      console.log('this is the response: ',response);
     })
   }
-      $scope.getApi();
-      // $scope.getAnotherApi();
+
+  $scope.directionReverseApi = function(toinput, frominput) {
+    mainService.directionApi(toinput, frominput).then(function(response) {
+      $scope.steps = response.data.routes[0].legs[0].steps;
+    })
+  }
+
 })
--- SecondNoServerProject/js/services/mainService.js
+++ SecondNoServerProject/js/services/mainService.js
@@ -1,13 +1,7 @@
 angular.module('journal').service('mainService', function($http) {
-  this.getApi = function(site) {
-    return $http({
-      method: 'GET',
-      url: 'https://maps.googleapis.com/maps/api/directions/json?origin=75+9th+Ave+New+York,+NY&destination=MetLife+Stadium+1+MetLife+Stadium+Dr+East+Rutherford,+NJ+07073&key=AIzaSyDIYdOLEBDJLUV6pd41h_PAdVhFoXZyyTo'
 
-    })
-  }
 
-this.getAnotherApi = function(originname, destname) {
+this.directionApi = function(originname, destname) {
 
   var key = 'AIzaSyDIYdOLEBDJLUV6pd41h_PAdVhFoXZyyTo';
   for (var i = 0; i < destname.length; i++) {
@@ -15,6 +9,12 @@ this.getAnotherApi = function(originname, destname) {
       destname = destname.slice(0,i) + "&" + destname.slice(i + 1);
     }
   }
+  for(var i = 0; i < originname.length; i++) {
+    if(originname.slice(i, i+1) === " ") {
+      originname = originname.slice(0,i) + "&" + originname.slice(i + 1);
+    }
+  }
+
   return $http({
     method: 'GET',
     url:'https://maps.googleapis.com/maps/api/directions/json?origin='+originname+'&destination='+destname+'&key='+key
