d12077ae2e13fa41b575a43a96f2dd8177d59377
akshar <aksharp99@gmail.com>
2016-10-09 15:32:49 -0600

    Initial

--- SecondNoServerProject/js/services/mainService.js
+++ SecondNoServerProject/js/services/mainService.js
@@ -6,12 +6,12 @@ this.directionApi = function(originname, destname) {
   var key = 'AIzaSyDIYdOLEBDJLUV6pd41h_PAdVhFoXZyyTo';
   for (var i = 0; i < destname.length; i++) {
     if(destname.slice(i, i+1) === " ") {
-      destname = destname.slice(0,i) + "&" + destname.slice(i + 1);
+      destname = destname.slice(0,i) + "+" + destname.slice(i + 1);
     }
   }
   for(var i = 0; i < originname.length; i++) {
     if(originname.slice(i, i+1) === " ") {
-      originname = originname.slice(0,i) + "&" + originname.slice(i + 1);
+      originname = originname.slice(0,i) + "+" + originname.slice(i + 1);
     }
   }
 
