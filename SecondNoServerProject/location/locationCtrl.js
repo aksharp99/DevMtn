653adefe04c470cd67a0660917034fb340706afa
akshar <aksharp99@gmail.com>
2016-10-10 16:29:00 -0600

    Initial

--- SecondNoServerProject/location/locationCtrl.js
+++
@@ -1,3 +0,0 @@
-angular.module('journal').controller('locationCtrl', function($scope) {
-  $scope.test = "This is the location state";
-})
--- SecondNoServerProject/price/priceCtrl.js
+++
@@ -1,3 +0,0 @@
-angular.module('journal').controller('priceCtrl', function($scope) {
-$scope.test = "This is the price state";
-})
