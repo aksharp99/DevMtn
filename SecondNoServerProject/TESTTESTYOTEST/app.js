773424f4f769cbd0bda795727b9267915c25d103
akshar <aksharp99@gmail.com>
2016-10-11 09:20:36 -0600

    Final

--- SecondNoServerProject/TESTTESTYOTEST/app.js
+++ SecondNoServerProject/TESTTESTYOTEST/app.js
@@ -1,1 +1,21 @@
-angular.module('journal', [])
+angular.module('journal', ['ui.router'])
+.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+
+
+  .state('demox',{
+    controller: 'demoxCtrl',
+    url: '/demox',
+    templateUrl: './demox.html'
+  })
+
+  .state('demoy', {
+    controller: 'demoyCtrl',
+    url: '/demoy',
+    templateUrl: './demoy.html'
+  })
+
+
+})
