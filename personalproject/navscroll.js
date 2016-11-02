79c61b53597345319b2aebf584bd75f11f7ea859
akshar <aksharp99@gmail.com>
2016-11-02 10:57:57 -0600

    added navbar scroll feature

---
+++ personalproject/navscroll.js
@@ -0,0 +1,18 @@
+;(function() {
+  var previousScroll = 0;
+  var navbar = document.getElementById('category-bar');
+  navClasses = navbar.classList;
+
+  window.addEventListener('scroll', function(e) {
+    var currentScroll = window.scrollY;
+    var isDown = currentScroll > previousScroll;
+
+    if (isDown && !navClasses.contains('scrolled')) {
+      navClasses.add('scrolled');
+    } else if (!isDown) {
+      navClasses.remove('scrolled');
+    }
+
+    previousScroll = currentScroll;
+  });
+}());
