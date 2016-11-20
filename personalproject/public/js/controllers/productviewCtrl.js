0c67d65e8915b4bdeb72eb636e27bccf17a18e32
akshar <aksharp99@gmail.com>
2016-11-20 14:52:54 -0700

    test

--- personalproject/public/js/controllers/productviewCtrl.js
+++ personalproject/public/js/controllers/productviewCtrl.js
@@ -1,11 +1,12 @@
 angular.module('myapp').controller('productviewCtrl', function($scope, productService, $stateParams) {
 
+console.log('this is $stateParams: ',$stateParams);
+console.log('this is $stateParams.id: ',$stateParams.id);
   $scope.getOneImage = function() {
     productService.getProductById($stateParams.id).then(function(response) {
       $scope.product = response[0];
       $scope.price = $scope.product.price;
-      console.log('this is the response in mainCtrl: ',response);
-      console.log('this is the scope product: ',response[0]);
+      console.log('this is the productViewCtrl response: ',response);
     })
   }
     $scope.getOneImage();
